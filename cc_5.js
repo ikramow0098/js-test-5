f3372fb11e0b1063a5244d99924448266f9b324a
Fotihjon <ikromov@usf.edu>
2025-02-09 18:37:13 -0500

    Task 2: Object Methods - Order Details

---
+++ cc_5.js
@@ -0,0 +1,15 @@
+// Task 1: Object Properties - Customer Profile
+const customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
+// Task 2: Object Methods - Order Details
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function () {
+        console.log(`Order ID: ${this.orderId}, Amount: $${this.totalAmount}, Status: ${this.status}`);
+    }
+};
+order.displayOrder();
