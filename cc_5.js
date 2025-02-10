bd8c3356d7ab7d52bef397124c943bf614693795
Fotihjon <ikromov@usf.edu>
2025-02-09 19:02:19 -0500

    Task 9: Function Expression - Discount Application

--- cc_5.js
+++ cc_5.js
@@ -47,3 +47,9 @@ function calculateTax(amount, taxRate) {
     return amount * taxRate;
 }
 console.log(`Tax on $100 at 10%: $${calculateTax(100, 0.1)}`);
+
+// Task 9: Function Expression - Discount Application
+const applyDiscount = function (price, discount) {
+    return price - (price * discount);
+};
+console.log(`Discounted Price: $${applyDiscount(100, 0.2)}`);
